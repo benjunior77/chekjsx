976afed03bb2998b0c9e72c243dc5248ba2aef13
benjunior77 <127311937+benjunior77@users.noreply.github.com>
2023-06-21 19:00:11 +0200

    Add files via upload

---
+++ Description.js
@@ -0,0 +1,11 @@
+import React from "react";
+import product from "../product";
+
+const Description = () => {
+ return (
+   <>
+     <h2>  {product.description} </h2>
+   </>
+ );
+};
+export default Description;
---
+++ Url.js
@@ -0,0 +1,11 @@
+import React from "react";
+import product from "../product";
+
+const Url = () => {
+ return (
+   <>
+     <img   src={product.url} ></img>
+   </>
+ );
+};
+export default Url ;
